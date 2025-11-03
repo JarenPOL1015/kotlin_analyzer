c1e52351de8ba6f85854695f6a416c3862486156
JarenPOL1015 <jarapazm@espol.edu.ec>
2025-11-03 12:40:24 -0500

    chore: Adding First Algorithm by Jaren Pazmiño

---
+++ algoritmo1.kt
@@ -0,0 +1,14 @@
+// Jaren Pazmiño
+
+// Esto es un comentario
+fun main() {
+    val x: Int = 10
+    var y = x + 5
+    val s: String = "Hola Mundo"
+
+    if (y > 10) {
+        println(s) // Imprime saludo
+    } else {
+        val z = y ?: 0
+    }
+}
