5e41e9fffbb1b2b7e741a3ac06d23b367be908a0
davexat <yisuscristh20@gmail.com>
2025-11-10 02:11:12 -0500

    chore: Improving algorithm 2

--- algoritmo2.kt
+++ algoritmo2.kt
@@ -1,14 +1,40 @@
 // David Sandoval
 
-// Programa que calcula el factorial de un número
-fun main() {
-    val n: Int = 5  // Número del que se quiere el factorial
-    var resultado = 1
+// Clase que representa un contador de números pares
+class Contador {
+    var totalPares = 0
 
-    // Bucle para calcular el factorial
-    for (i in 1..n) {
-        resultado *= i
+    fun contarHasta(n: Int) {
+        // Iteramos del 1 al número dado usando un for con rango
+        for (i in 1..n) {
+            // Si el número es par
+            if (i % 2 == 0) {
+                totalPares = totalPares + 1
+                println(i)  // Imprime el número par
+            }
+        }
+    }
+}
+
+// Programa principal
+fun main() {
+    // Creamos una instancia de la clase
+    val c = Contador()
+
+    // Leemos un número desde input
+    var limite = readLine()
+
+    // Convertimos el input a entero si no es nulo
+    if (limite != null) {
+        // Simple casting simulado con expression
+        var n = limite.toInt()  // Consideramos que `toInt()` se ignora en el parser
+        c.contarHasta(n)
     }
 
-    println("El factorial de $n es $resultado") // Muestra el resultado
+    // Bucle while para mostrar mensaje repetido
+    var x = 0
+    while (x < 3) {
+        println("Repetición: " + x)
+        x = x + 1
+    }
 }
